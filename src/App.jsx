051c676d11a2c61dcfc514d88f94893dffd3d516
Abrosss <nataroro5@gmail.com>
2023-02-23 20:26:33 +0600

    added preview feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './css/styles.scss'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Preview from './assets/icon-show-preview.svg'
+import HidePreview from './assets/icon-hide-preview.svg'
 import { useState } from 'react'
 function App() {
 const [expanded, setExpanded] = useState(false)
+const [previewFull, setpreviewFull] = useState(false)
 console.log(expanded)
   return (
     <>
@@ -15,18 +17,18 @@ console.log(expanded)
     <section className={expanded ? 'container expand' : 'container'}>
     <Header expanded={expanded} func={() => setExpanded(!expanded)}/>
     <section className='editor-space'>
-      <section className='markdown part'>
+      <section className={previewFull ? 'markdown part hidden' : 'markdown part'}>
         <div className='header-editor'>
           <span>MARKDOWN</span>
         </div>
         <textarea>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quidem placeat nostrum distinctio voluptates ratione laudantium quaerat delectus maiores quo nam perspiciatis quos repellendus iste, et, earum molestias accusantium adipisci.</textarea>
       </section>
-       <section className='preview part'>
+       <section className={previewFull ? 'preview part full' : 'preview part'}>
        <div className='header-editor'>
    
           <span>PREVIEW</span>
-          <div className='clickable'>
-          <img src={Preview} alt='preview icon'></img>
+          <div onClick={() => setpreviewFull(!previewFull)} className='clickable'>
+          <img src={previewFull ? HidePreview : Preview} alt='preview icon'></img>
           </div>
         </div>
        </section>
